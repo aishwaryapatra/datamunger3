ff092e5f4e4a39649cc1c7ad3b28a18529b1dcd7
aispatr1 <aispatr1@in.ibm.com>
2019-12-10 23:24:55 +0530

    mycomit

--- src/com/stackroute/datamunger/query/Header.java
+++ src/com/stackroute/datamunger/query/Header.java
@@ -1,14 +1,27 @@
 package com.stackroute.datamunger.query;
 
 public class Header {
+	String[] arr;
+	
 
 	/*
 	 * This class should contain a member variable which is a String array, to hold
 	 * the headers.
 	 */
 	
+	public Header(String[] arr) {
+		super();
+		this.arr = arr;
+	}
+
+
+	
+
 	public String[] getHeaders() {
-		return null;
+		return this.arr;
+	}
+	public void setHeaders(String[] arr) {
+		this.arr=arr;
 	}
 
 }
--- src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
+++ src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
@@ -1,17 +1,26 @@
 package com.stackroute.datamunger.reader;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import com.stackroute.datamunger.query.DataTypeDefinitions;
 import com.stackroute.datamunger.query.Header;
 
 public class CsvQueryProcessor extends QueryProcessingEngine {
 
+	File fileName;
+
 	// Parameterized constructor to initialize filename
 	public CsvQueryProcessor(String fileName) throws FileNotFoundException {
+		this.fileName=new File(fileName);
 
 	}
+	
 
 	/*
 	 * Implementation of getHeader() method. We will have to extract the headers
@@ -21,11 +30,21 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	
 	@Override
 	public Header getHeader() throws IOException {
+		BufferedReader bfr=new BufferedReader(new FileReader(fileName));
+		String text=bfr.readLine();
+		String[] ar=text.split(",");
+		if(ar.length!=-1) {
+		Header hd=new Header(ar);
+		return hd;
+		}
+		else {
+			return null;
+		}
 
 		// read the first line
 
 		// populate the header object with the String array containing the header names
-		return null;
+		
 	}
 
 	/**
@@ -49,7 +68,63 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	
 	@Override
 	public DataTypeDefinitions getColumnType() throws IOException {
+		try {
+		int i = 0;
+		//String type = null;
+		BufferedReader bf=new BufferedReader(new FileReader(fileName));
+		
+		String text=bf.readLine();
+		int len=text.split(",").length;
+		text=bf.readLine();
+		String[] ar=text.split(",");
+		String[] arr= new String[len];
+		//if(ar.length != -1) {
+		for(String s:ar ) {
+			try {
+			int x=Integer.parseInt(s);
+			arr[i]="java.lang.Integer";
+			}
+			catch(Exception e1) {
+				try {
+					double x=Double.parseDouble(s);
+					arr[i]="java.lang.Double";
+				}
+				catch(Exception e2) {
+//					if(ar[i] instanceof String)
+						s="java.lang.String";
+				}
+			} 
+			finally {
+				i++;
+			}
+		}
+				for(i=0;i<arr.length;i++) {
+					if(arr[i]==null)
+						arr[i]="java.lang.String";
+				}
+			
+		
+		
+		
+				DataTypeDefinitions df=new DataTypeDefinitions(arr);
+				bf.close();
+	
 
-		return null;
+		return df;
+		
+		}
+		
+		catch(Exception e) {
+			DataTypeDefinitions df = new DataTypeDefinitions(new String[0]);
+			return df;
+		
+		}
 	}
 }
+		
+		
+	
+
+
+		
+
